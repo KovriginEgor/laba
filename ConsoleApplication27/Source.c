66f75655c4ab73da0c1d307cd86fa1c4b1d0a49f
Paprikman <starlight1213@yandex.ru>
2017-06-05 21:07:42 +0500

    first commit

---
+++ ConsoleApplication27/Source.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	FILE *fp, *fo;
+	if (argv[1] == NULL) return 0;
+	/*if (argv[1] == NULL) fp = fopen("lab_10_text.txt", "r");
+	else*/ fp = fopen(argv[1], "r");
+	fo = fopen("output1.txt", "w");
+	char str[20];
+	char c;
+	char t;
+	
+	while ((t = fgetc(fp)) != EOF) {
+		fseek(fp, -1, 1);
+		while (((c = fgetc(fp)) != '\n')&&(c != EOF)) {
+			fseek(fp, -1, 1);
+			fscanf(fp, "%s", &str);
+			fprintf(fo,"%8s ", str);
+		}
+		fprintf(fo,"\n");
+	}
+
+	fclose(fp);
+	fclose(fo);
+
+	getch();
+	return 0;
+}
